23644fd0348ff4b5229849a12451b7cd66af50ba
Felipe <huertasguarinfelipez2@gmail.com>
2024-08-07 09:45:33 -0500

    Agregar archivos de proyecto.

---
+++ PrimeraAplicacion/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    static void pruebaBucles()
+    {
+        //Bucle for
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("el valor de i:" + i);
+        }
+    }
+
+    static void pruebaEstructuraControl()
+    {
+        //
+    }
+
+    static void pruebaVariables()
+    {
+        //
+    }
+    private static void Main(string[] args)
+    {
+        // Bucle do-while
+        int k = 0;
+        do
+        {
+            Console.WriteLine("Do-While loop, k = " + k);
+            k++;
+        } while (k < 5);
+
+        k = 0;
+        
+        while(k < 5)
+        {
+            Console.WriteLine("el valor de k es = " + k);
+            k++;
+        }
+
+        double temperatura = 32.5;
+
+        int temperaturaVillavo;
+
+        temperaturaVillavo = (int)temperatura;
+
+        Console.WriteLine(temperaturaVillavo);
+        Console.WriteLine(temperatura);
+    }
+}
