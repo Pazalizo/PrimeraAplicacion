8bb901409be672abb0ec2a74f2f18e232bec9e9c
Felipe <huertasguarinfelipez2@gmail.com>
2024-08-07 16:52:23 -0500

    poco a poco

--- PrimeraAplicacion/Program.cs
+++ PrimeraAplicacion/Program.cs
@@ -1,48 +1,65 @@
-﻿internal class Program
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
 {
     static void pruebaBucles()
     {
-        //Bucle for
+        // Bucle for
         for (int i = 0; i < 5; i++)
         {
-            Console.WriteLine("el valor de i:" + i);
+            Console.WriteLine("el valor de i: " + i);
         }
-    }
 
-    static void pruebaEstructuraControl()
-    {
-        //
-    }
-
-    static void pruebaVariables()
-    {
-        //
-    }
-    private static void Main(string[] args)
-    {
-        // Bucle do-while
+        // Bucle While
         int k = 0;
+        while (k < 5)
+        {
+            Console.WriteLine($"el valor de k: {k}");
+        }
+
+        // Bucle Do While
+        int d = 0;
         do
         {
-            Console.WriteLine("Do-While loop, k = " + k);
-            k++;
-        } while (k < 5);
-
-        k = 0;
-        
-        while(k < 5)
-        {
-            Console.WriteLine("el valor de k es = " + k);
-            k++;
+            Console.WriteLine($"el valor de i: {d}");
+            d++;
         }
+        while (d < 5);
 
-        double temperatura = 32.5;
+    }
+    static void pruebaEstructuraControl()
+    {
+            // if
+            int i = 0;
+            if (i < 5)
+            {
+                Console.WriteLine(i);
+            }
+            switch (i)
+            {
+                case 0:
+                    Console.WriteLine("el numero de i es 0");
+                    break;
+                case 1:
+                    Console.WriteLine("el numero de i es 1");
+                    break;
+                default:
+                    Console.WriteLine("el numero no es ni 0 ni 1");
+                    break;
+            }
+    }
+    
 
-        int temperaturaVillavo;
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Introduce el primer numero");
+        int num1 = int.Parse(Console.ReadLine());
 
-        temperaturaVillavo = (int)temperatura;
+        Console.WriteLine("Introduce el segundo numero");
+        int num2 = int.Parse(Console.ReadLine());
 
-        Console.WriteLine(temperaturaVillavo);
-        Console.WriteLine(temperatura);
+        int resultado = num1 + num2;
+
+        Console.WriteLine($"El resultado de la suma es: {resultado}");
     }
 }
