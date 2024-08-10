772a2d8a098e15c65027af4081ce1d4ec159d730
Felipe <huertasguarinfelipez2@gmail.com>
2024-08-09 22:48:41 -0500

    olawenas owo

--- PrimeraAplicacion/Program.cs
+++ PrimeraAplicacion/Program.cs
@@ -29,39 +29,48 @@ internal class Program
     }
     static void pruebaEstructuraControl()
     {
-            // if
-            int i = 0;
-            if (i < 5)
-            {
-                Console.WriteLine(i);
-            }
-            switch (i)
-            {
-                case 0:
-                    Console.WriteLine("el numero de i es 0");
-                    break;
-                case 1:
-                    Console.WriteLine("el numero de i es 1");
-                    break;
-                default:
-                    Console.WriteLine("el numero no es ni 0 ni 1");
-                    break;
-            }
+        // if
+        int i = 0;
+        if (i < 5)
+        {
+            Console.WriteLine(i);
+        }
+        switch (i)
+        {
+            case 0:
+                Console.WriteLine("el numero de i es 0");
+                break;
+            case 1:
+                Console.WriteLine("el numero de i es 1");
+                break;
+            default:
+                Console.WriteLine("el numero no es ni 0 ni 1");
+                break;
+        }
     }
 
     static int suma(int numero1, int numero2)
     {
-        int resultado = numero1+numero2 ;
-        return resultado ;
+        int resultado = numero1 + numero2;
+        return resultado;
     }
 
     static void segundoMetodo()
     {
         Console.WriteLine("hello world");
     }
-    
 
-    private static void Main(string[] args)
+    // Prueba de sobrecarga
+
+    static int Suma(int numero1, int numero2) => numero1 + numero2;
+
+    static double Suma(double numero1, double numero2) => numero1 + numero2;
+
+    static int Suma(int numero1, int numero2, int numero3) => numero1 + numero2 + numero3;
+
+    // Prueba de Ingreso de Datos
+
+    static void ingresoDatos()
     {
         Console.WriteLine("Introduce el primer numero");
         int num1 = int.Parse(Console.ReadLine());
@@ -69,8 +78,17 @@ internal class Program
         Console.WriteLine("Introduce el segundo numero");
         int num2 = int.Parse(Console.ReadLine());
 
-        int resultado = suma(num1,num2);
+        int resultado = suma(num1, num2);
 
         Console.WriteLine($"El resultado de la suma es: {resultado}");
+    }
+
+    private static void Main(string[] args)
+    {
+        int numero1 = 12;
+        int numero2 = 25;
+
+        Console.WriteLine(Suma(numero1, numero2));
+
     }
 }
