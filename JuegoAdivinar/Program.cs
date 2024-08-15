bbddb0bc0664b2e9a33ca0bf1509a31577c470c2
Felipe <huertasguarinfelipez2@gmail.com>
2024-08-14 21:33:40 -0500

    Fix/Arreglado el juego de pedir un numero

--- JuegoAdivinar/Program.cs
+++ JuegoAdivinar/Program.cs
@@ -2,7 +2,7 @@
 {
     static int solicitarNumero(int numeroMenor, int numeroMayor)
     {
-        Console.WriteLine($"Ingrese un numero del {numeroMenor} al {numeroMayor}");
+        Console.WriteLine($"El numero esta entre {numeroMenor} y {numeroMayor}");
         int numero = int.Parse(Console.ReadLine());
         return numero;
     }
@@ -15,12 +15,33 @@
     static void juegoAdivinar()
     {
         Console.WriteLine("Bienvenido al juego");
-        //int numero = solicitarNumero();
+        int numeroMenor = 1;
+        int numero = 0;
+        int numeroMayor = 100;
         int numeroAleatorio = generarNumeroAleatorio();
         int contador = 0;
+        do
+        {
+            numero = solicitarNumero(numeroMenor, numeroMayor);
+            contador++;
+            if (numero == numeroAleatorio)
+            {
+                Console.WriteLine($"Ganaste el juego el numero era {numero}, ganaste con {contador} intentos");
+            }
+            else if (numero < numeroAleatorio)
+            {
+                numeroMenor = numero;
+            }
+            else
+            {
+                numeroMayor = numero;
+            }
+        }
+        while (numero != numeroAleatorio);
     }
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        juegoAdivinar();
     }
 }
