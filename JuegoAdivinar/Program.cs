4109367b80edba135c073b44705346a1e5e3f336
Felipe <huertasguarinfelipez2@gmail.com>
2024-08-16 10:00:07 -0500

    Fix/Se corrigio la funcion de generar numeros aleatorios

--- JuegoAdivinar/Program.cs
+++ JuegoAdivinar/Program.cs
@@ -6,10 +6,10 @@
         int numero = int.Parse(Console.ReadLine());
         return numero;
     }
-    static int generarNumeroAleatorio()
+    static int generarNumeroAleatorio(int numeroMenor, int numeroMayor)
     {
         Random random = new Random();
-        int randomNumber = random.Next(1, 101);
+        int randomNumber = random.Next(numeroMenor, numeroMayor +1);
         return randomNumber;
     }
     static void juegoAdivinar()
@@ -17,13 +17,21 @@
         Console.WriteLine("Bienvenido al juego");
         int numeroMenor = 1;
         int numero = 0;
-        int numeroMayor = 100;
-        int numeroAleatorio = generarNumeroAleatorio();
+        int numeroMayor = 10;
+        int numeroAleatorio = generarNumeroAleatorio(numeroMenor, numeroMayor);
         int contador = 0;
         do
         {
-            numero = solicitarNumero(numeroMenor, numeroMayor);
-            contador++;
+            try
+            {
+                numero = solicitarNumero(numeroMenor, numeroMayor);
+                contador++;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ingrese un numero entero, que no lee?");
+            }
+            
             if (numero == numeroAleatorio)
             {
                 Console.WriteLine($"Ganaste el juego el numero era {numero}, ganaste con {contador} intentos");
