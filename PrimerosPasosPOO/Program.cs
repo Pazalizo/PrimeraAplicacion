040ff44de19292823bc5cb3ca043e9f3383ae2d8
Felipe <huertasguarinfelipez2@gmail.com>
2024-08-21 16:33:02 -0500

    Feature/uso de encapsulamiento y creacion de construcctores

--- PrimerosPasosPOO/Program.cs
+++ PrimerosPasosPOO/Program.cs
@@ -8,10 +8,39 @@
             return pi * radio*radio;
         }
     }
+
+    class Persona
+    {
+        private string nombrePersona;
+        private int edad;
+        private int cedula;
+        public Persona()
+        {
+            this.edad = edad;
+        }
+    }
+
+    class Carro
+    {
+        private int cantidadllantas;
+        private int cantidadPuertas;
+        public Carro()
+        {
+            cantidadllantas = 4;
+            cantidadPuertas = 2;
+        }
+
+        public String infoCarro()
+        {
+            return $"el carro tiene {cantidadllantas} llantas y tiene {cantidadPuertas} puertas";
+        }
+    }
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         Circulo circulo1 = new Circulo();
-        Console.WriteLine($"{circulo1.CalcularArea(53)}");
+        Carro carro = new Carro();
+        Console.WriteLine(circulo1.CalcularArea(53));
+        Console.WriteLine(carro.infoCarro());
     }
 }
